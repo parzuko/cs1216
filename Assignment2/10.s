e80508c72d621adbf37574758ecc29d4b3b15007
parzuko <jivansh17@gmail.com>
2022-10-16 23:10:24 +0530

    :sparkles: add assignment 2

---
+++ Assignment2/10.s
@@ -0,0 +1,14 @@
+# explain the following snuippets of code
+
+addi $sp, $sp, -12 # sp = sp - 12
+
+# access array elements in MIPS
+sw $t1, 8($sp) # store t1 at address sp + 8
+sw $t0, 4($sp) # store t0 at address sp + 4
+sw $s0, 0($sp) # store t2 at address sp
+
+
+addi $sp, $sp, -12 # sp = sp - 12
+sw $t1, 8($sp) # store t1 at address sp + 8
+sw $t0, 4($sp) # store t0 at address sp + 4
+sw $s0, 0($sp) # store t2 at address sp
